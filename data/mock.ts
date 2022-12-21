92dce20657a8e0e1b1370c45fc75d889beff9ce9
Alex Henry <5092131+alxHenry@users.noreply.github.com>
2022-12-20 23:04:15 -0800

    Bulk up mock data

--- data/mock.ts
+++ data/mock.ts
@@ -1,5 +1,12 @@
 import { PlayersById } from "../types/Players";
 
+export const MY_MOCK_TEAM_ID = "dummy-mock-team";
+export const MY_INITIAL_TEAM = {
+  name: "My team",
+  id: MY_MOCK_TEAM_ID,
+  playerIds: [],
+};
+
 export const PLAYERS_MOCK: PlayersById = {
   "1": {
     id: "1",
@@ -19,7 +26,80 @@ export const PLAYERS_MOCK: PlayersById = {
     },
     team: "NYY",
   },
+  "3": {
+    id: "3",
+    name: "Aaron Judge",
+    stats: {
+      avg: { id: "avg", name: "avg", value: 0.29, isHigherBetter: true },
+      hr: { id: "hr", name: "hr", value: 60, isHigherBetter: true },
+    },
+    team: "NYY",
+  },
+  "4": {
+    id: "4",
+    name: "Aaron Judge",
+    stats: {
+      avg: { id: "avg", name: "avg", value: 0.29, isHigherBetter: true },
+      hr: { id: "hr", name: "hr", value: 60, isHigherBetter: true },
+    },
+    team: "NYY",
+  },
+  "5": {
+    id: "5",
+    name: "Aaron Judge",
+    stats: {
+      avg: { id: "avg", name: "avg", value: 0.29, isHigherBetter: true },
+      hr: { id: "hr", name: "hr", value: 60, isHigherBetter: true },
+    },
+    team: "NYY",
+  },
+  "6": {
+    id: "6",
+    name: "Aaron Judge",
+    stats: {
+      avg: { id: "avg", name: "avg", value: 0.29, isHigherBetter: true },
+      hr: { id: "hr", name: "hr", value: 60, isHigherBetter: true },
+    },
+    team: "NYY",
+  },
+  "7": {
+    id: "7",
+    name: "Aaron Judge",
+    stats: {
+      avg: { id: "avg", name: "avg", value: 0.29, isHigherBetter: true },
+      hr: { id: "hr", name: "hr", value: 60, isHigherBetter: true },
+    },
+    team: "NYY",
+  },
+  "8": {
+    id: "8",
+    name: "Aaron Judge",
+    stats: {
+      avg: { id: "avg", name: "avg", value: 0.29, isHigherBetter: true },
+      hr: { id: "hr", name: "hr", value: 60, isHigherBetter: true },
+    },
+    team: "NYY",
+  },
+  "9": {
+    id: "9",
+    name: "Aaron Judge",
+    stats: {
+      avg: { id: "avg", name: "avg", value: 0.29, isHigherBetter: true },
+      hr: { id: "hr", name: "hr", value: 60, isHigherBetter: true },
+    },
+    team: "NYY",
+  },
+  "10": {
+    id: "10",
+    name: "Aaron Judge",
+    stats: {
+      avg: { id: "avg", name: "avg", value: 0.29, isHigherBetter: true },
+      hr: { id: "hr", name: "hr", value: 60, isHigherBetter: true },
+    },
+    team: "NYY",
+  },
 };
+
 export const getMockPlayerData = (): Promise<PlayersById> => {
   return new Promise((resolve) => {
     resolve(PLAYERS_MOCK);
