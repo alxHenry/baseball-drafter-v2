7bbb882fd5c48e3f487de427b11fda44d9a9467d
Alex Henry <5092131+alxHenry@users.noreply.github.com>
2022-12-22 14:49:24 -0800

    Cleanup boilerplate

--- app/draft/page.tsx
+++ app/draft/page.tsx
@@ -11,11 +11,6 @@ export default async function Page() {
   return (
     <>
       <h1 className={styles.header}>Draft</h1>
-      <div className={styles.draftContainer}>
-        <div className={styles.column}>Your team</div>
-        <div className={styles.column}>Available Players</div>
-        <div className={`${styles.column} ${styles.actions}`}>Actions</div>
-      </div>
       <DraftPlayerList battersById={transformedBatterById} />
       <TeamDisplay />
     </>
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body className={styles.body}>
-        <nav className={styles.nav}>Nav</nav>
         <main className={styles.main}>{children}</main>
-        <footer className={styles.footer}>Footer</footer>
       </body>
     </html>
   );
