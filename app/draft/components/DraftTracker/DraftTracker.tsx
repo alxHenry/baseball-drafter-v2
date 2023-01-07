9cf66e02d5445562755c6efc1d488259178a1d59
Alex Henry <5092131+alxHenry@users.noreply.github.com>
2023-01-06 23:21:52 -0800

    Add a draft tracker

--- app/draft/components/DraftTracker/DraftTracker.tsx
+++ app/draft/components/DraftTracker/DraftTracker.tsx
@@ -2,21 +2,25 @@
 
 import styles from "./DraftTracker.module.css";
 
-import { FC, memo } from "react";
+import { FC, memo, useRef } from "react";
 import shallow from "zustand/shallow";
 import { useStore } from "../../../../data/stores/store";
 import DraftTrackerCard from "./DraftTrackerCard";
-import DraftTrackerEndArrow from "./DraftTrackerEndArrow";
+import DraftTrackerArrow from "./DraftTrackerArrow";
 
 interface Props {}
 
 const DraftTracker: FC<Props> = () => {
+  const scrollRef = useRef(null);
   const teamIds = useStore((state) => Object.keys(state.teamsSlice.teamsById), shallow);
   const renderedCards = teamIds.map((teamId) => <DraftTrackerCard key={teamId} teamId={teamId} />);
-  const endScrollArrow = <DraftTrackerEndArrow />;
+
+  const startScrollArrow = <DraftTrackerArrow isStartArrow={true} scrollRef={scrollRef} />;
+  const endScrollArrow = <DraftTrackerArrow isStartArrow={false} scrollRef={scrollRef} />;
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} ref={scrollRef}>
+      {startScrollArrow}
       {renderedCards}
       {endScrollArrow}
     </div>
