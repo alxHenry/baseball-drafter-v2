464d74c3b66fbf2422248a8dafe68365f6d85f76
Alex Henry <5092131+alxHenry@users.noreply.github.com>
2023-01-08 20:52:27 -0800

    Draft tracker arrows (#3)
    * WIP draft tracker floating arrows
    
    * Add a draft tracker
    
    * Update to Next.js 13.1
    
    * Support relative and asbolute stat display

--- app/draft/components/StatCell.tsx
+++ app/draft/components/StatCell.tsx
@@ -3,13 +3,18 @@ import type { Stat } from "../../../data/stores/playersSlice";
 
 import { memo } from "react";
 import { Cell } from "@table-library/react-table-library";
+import StatDisplayValue from "./StatDisplayValue";
 
 interface Props {
   readonly stat: Stat;
 }
 
 const StatCell: FC<Props> = ({ stat }) => {
-  return <Cell>{stat.abs}</Cell>;
+  return (
+    <Cell>
+      <StatDisplayValue stat={stat} />
+    </Cell>
+  );
 };
 
 export default memo(StatCell);
