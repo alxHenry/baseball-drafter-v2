bc18a0e70ffa310cd95754df569789b5ae82a6c6
Alex Henry <5092131+alxHenry@users.noreply.github.com>
2023-01-22 09:46:55 -0800

    Allow sorting by realtive or absolute stat

--- app/draft/components/useTableSortFns.ts
+++ app/draft/components/useTableSortFns.ts
@@ -4,13 +4,17 @@ import { useMemo } from "react";
 import { useStore } from "../../../data/stores/store";
 import { StatId } from "../../../data/types/stats";
 
-const playerStatReducer = (agg: Record<string, SortFn>, stat: string) => {
+const getPlayerStatReducer = (isShowingRelative: boolean) => (agg: Record<string, SortFn>, stat: string) => {
   agg[stat] = (array) =>
     array.sort((a, b) => {
       const playerA = a as Player;
       const playerB = b as Player;
 
-      return playerB.stats[stat as StatId]!.rel - playerA.stats[stat as StatId]!.rel;
+      if (isShowingRelative) {
+        return playerB.stats[stat as StatId]!.rel - playerA.stats[stat as StatId]!.rel;
+      } else {
+        return playerB.stats[stat as StatId]!.abs - playerA.stats[stat as StatId]!.abs;
+      }
     });
 
   return agg;
@@ -21,20 +25,21 @@ export const useTableSortFns = (): Record<string, SortFn> => {
   const pitcherStats = useStore((state) => state.draftSlice.pitcherStatConfigsById);
   const requiredStats = useStore((state) => state.draftSlice.requiredStatConfigsById);
   const tableDisplayMode = useStore((state) => state.draftSlice.currentTableDisplayMode);
+  const isShowingRelative = useStore((state) => state.draftSlice.showRelativeStatValues);
 
   return useMemo(() => {
     if (tableDisplayMode === "All") {
-      return Object.keys(requiredStats).reduce<Record<string, SortFn>>(playerStatReducer, {});
+      return Object.keys(requiredStats).reduce<Record<string, SortFn>>(getPlayerStatReducer(isShowingRelative), {});
     } else if (tableDisplayMode === "Batters") {
       return [...Object.keys(batterStats), ...Object.keys(requiredStats)].reduce<Record<string, SortFn>>(
-        playerStatReducer,
+        getPlayerStatReducer(isShowingRelative),
         {}
       );
     } else {
       return [...Object.keys(pitcherStats), ...Object.keys(requiredStats)].reduce<Record<string, SortFn>>(
-        playerStatReducer,
+        getPlayerStatReducer(isShowingRelative),
         {}
       );
     }
-  }, [batterStats, pitcherStats, requiredStats, tableDisplayMode]);
+  }, [batterStats, isShowingRelative, pitcherStats, requiredStats, tableDisplayMode]);
 };
