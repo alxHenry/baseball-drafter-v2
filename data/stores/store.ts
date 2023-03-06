d237bc5c4c9cf315781637f18f935a266b6a8bcb
Alex Henry <5092131+alxHenry@users.noreply.github.com>
2023-03-05 19:53:34 -0800

    Use the suggested currying zustand syntx

--- data/stores/store.ts
+++ data/stores/store.ts
@@ -21,7 +21,7 @@ export type StoreSet = (
 ) => void;
 export type StoreGet = () => Store;
 
-export const useStore = create<Store>((set: StoreSet, get: StoreGet) => ({
+export const useStore = create<Store>()((set: StoreSet, get: StoreGet) => ({
   draftSlice: getDraftSliceDefinitions(set, get),
   playersSlice: getPlayersSliceDefinitions(set, get),
   rotoRankingsSlice: getRotoRankingsSliceDefinitions(set, get),
