b1728b950d2d36e71e5b9eb892464575b3dc0262
Alex Henry <5092131+alxHenry@users.noreply.github.com>
2023-03-13 22:37:28 -0700

    Update ChatGPT algorithms. Something is up as neither works and if you overdraft a position it will spit out nothing

--- app/draft/components/Team/generateOptimalLineup.ts
+++ app/draft/components/Team/generateOptimalLineup.ts
@@ -1,14 +1,13 @@
 import { Player } from "../../../../data/stores/playersSlice";
 import { PositionId } from "../../../../data/types/positions";
 
-export type TeamLineup = Partial<Record<PositionId, Player>>;
+export type TeamLineup = Partial<Record<PositionId, Player[]>>;
 
 interface GenerateOptimalLineupArgs {
   readonly players: Player[];
   readonly positionRequirements: Record<PositionId, number>;
 }
 
-// ChatGPT wrote this - incredible
 export const generateOptimalLineup = ({ players, positionRequirements }: GenerateOptimalLineupArgs): TeamLineup => {
   // Create an array of available players for each position
   const playersByPosition: Partial<Record<PositionId, Player[]>> = {};
@@ -40,10 +39,16 @@ export const generateOptimalLineup = ({ players, positionRequirements }: Generat
     positionPermutations.forEach((permutation) => {
       const lineup: TeamLineup = { ...selectedLineup };
       permutation.forEach((player, index) => {
-        lineup[positionId as PositionId] = player;
+        if (!lineup[positionId as PositionId]) {
+          lineup[positionId as PositionId] = [];
+        }
+        lineup[positionId as PositionId]!.push(player);
         if (index === permutation.length - 1) {
           // This is the last position in the permutation, so calculate the total value of the lineup
-          const lineupValue = Object.values(lineup).reduce((total, player) => total + (player.stats.worth.abs || 0), 0);
+          const lineupValue = Object.values(lineup).reduce(
+            (total, players) => total + players.reduce((sum, player) => sum + (player.stats.worth.abs || 0), 0),
+            0
+          );
           if (lineupValue > bestValue) {
             bestValue = lineupValue;
             bestLineup = lineup;
@@ -56,7 +61,9 @@ export const generateOptimalLineup = ({ players, positionRequirements }: Generat
     selectedLineup = bestLineup;
     Object.keys(selectedLineup).forEach((positionId) => {
       remainingRequirements[positionId as PositionId] -=
-        positionId === positionId && selectedLineup[positionId as PositionId] !== undefined ? 1 : 0;
+        positionId === positionId && selectedLineup[positionId as PositionId] !== undefined
+          ? selectedLineup[positionId as PositionId]!.length
+          : 0;
     });
   });
 
@@ -72,8 +79,8 @@ const getPermutations = <T>(items: T[], length: number): T[][] => {
   const permutations: T[][] = [];
   items.forEach((item, index) => {
     const remainingItems = items.slice(0, index).concat(items.slice(index + 1));
-    const subPermutations = getPermutations(remainingItems, length - 1);
-    subPermutations.forEach((permutation) => {
+    const innerPermutations = getPermutations(remainingItems, length - 1);
+    innerPermutations.forEach((permutation) => {
       permutations.push([item].concat(permutation));
     });
   });
--- app/draft/components/Team/generateOptimalLineupDP.ts
+++ app/draft/components/Team/generateOptimalLineupDP.ts
@@ -1,26 +1,29 @@
 import { Player } from "../../../../data/stores/playersSlice";
 import { PositionId } from "../../../../data/types/positions";
 
-type PositionRequirements = Record<PositionId, number>;
 export type TeamLineup = Partial<Record<PositionId, Player[]>>;
 
 interface GenerateOptimalLineupArgs {
   readonly players: Player[];
-  readonly positionRequirements: PositionRequirements;
+  readonly positionRequirements: Record<PositionId, number>;
 }
 
+// ChatGPT generated this, but it sin't quite right. Try drafting two catchers. Runtime O(NM^2)
 export const generateOptimalLineupDP = ({ players, positionRequirements }: GenerateOptimalLineupArgs): TeamLineup => {
   const n = players.length;
   const p = Object.keys(positionRequirements).length;
-  const k = Math.max(...Object.values(positionRequirements));
   const table: number[][] = Array.from({ length: n + 1 }, () => Array.from({ length: p }, () => 0));
   const selections: number[][] = Array.from({ length: n + 1 }, () => Array.from({ length: p }, () => -1));
 
   // Fill in the table row by row
   for (let i = 1; i <= n; i++) {
     const player = players[i - 1];
+    const eligiblePositions = player.position;
     for (let j = 0; j < p; j++) {
       const positionId = Object.keys(positionRequirements)[j] as PositionId;
+      if (!eligiblePositions.includes(positionId)) {
+        continue; // Skip this position if the player is not eligible for it
+      }
       const numSelected = Object.values(selections[i - 1]).filter((s) => s === j).length;
       const remainingRequirements = positionRequirements[positionId] - numSelected;
 
@@ -40,25 +43,33 @@ export const generateOptimalLineupDP = ({ players, positionRequirements }: Gener
   }
 
   // Backtrack through the table to determine the selected lineup
-  const selectedLineup: TeamLineup = {};
-  const playerCounts: PositionRequirements = { ...positionRequirements };
-  for (let i = n; i > 0; i--) {
-    let j = p - 1;
-    while (j >= 0) {
-      const prevJ = selections[i][j];
-      if (prevJ !== -1) {
-        const positionId = Object.keys(positionRequirements)[j] as PositionId;
-        const player = players[i - 1];
-        if (!selectedLineup[positionId]) {
-          selectedLineup[positionId] = [];
-        }
-        selectedLineup[positionId]!.push(player);
-        playerCounts[positionId]--;
-        j = prevJ;
+  const selectedLineup = Object.keys(positionRequirements).reduce<TeamLineup>((agg, positionId) => {
+    agg[positionId as PositionId] = [];
+    return agg;
+  }, {});
+
+  let i = n;
+  let j = p - 1;
+  while (i > 0 && j >= 0) {
+    const prevJ = selections[i][j];
+    if (prevJ !== -1) {
+      const positionId = Object.keys(positionRequirements)[j] as PositionId;
+      const player = players[i - 1];
+      if (!selectedLineup[positionId]) {
+        selectedLineup[positionId] = [player];
       } else {
-        j--;
+        selectedLineup[positionId]!.push(player);
       }
+      i--;
+      j = prevJ;
+    } else {
+      j--;
     }
+  }
+
+  // Reverse the player arrays in the lineup to get them in the correct order
+  for (const positionId of Object.keys(selectedLineup)) {
+    selectedLineup[positionId as PositionId]!.reverse();
   }
 
   return selectedLineup;
